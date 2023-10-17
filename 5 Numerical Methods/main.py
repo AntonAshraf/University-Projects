b2d505bdb69a12c9774bae859b60c8bc47992c9b
Anton Ashraf <anton0ashraf@gmail.com>
2023-10-17 03:11:34 +0300

    make only 2 fileds to fill between them

--- 5 Numerical Methods/main.py
+++ 5 Numerical Methods/main.py
@@ -9,7 +9,7 @@ from methods.newton_raphson import newton_raphson
 from methods.simple_fixed_point_iteration import simple_fixed_point_iteration
 
 # Define a regular expression pattern to match numbers followed by "x"
-pattern = r'(\d+)x'
+pattern = r'(\d+)[xX]'
 
 def fields_error():
     result_label.config(text="Please fill all the fields", fg="red")
@@ -36,56 +36,38 @@ def calculate_root():
     modified_input = re.sub(pattern, replace, equation) # Replace "2x" with "2*x"
     f = lambda x: eval(modified_input)
 
-    if method == "Bisection":
-        try:
-            a = a_entry.get()
-            b = b_entry.get()
+    try:
+        a = a_entry.get()
+        b = b_entry.get()
+        
+        if method in ["Bisection", "False Position", "Secant"]:
             if not a or not b:
                 fields_error()
                 return
-            a = float(a)
-            b = float(b)
-            result, error , msg = bisection(f, a, b, float(tolerance), int(max_iterations))
-        except Exception as e:
-            result_label.config(text="Error: {}".format(e), fg="red")
-            error_label.config(text="")
-            info_label.config(text="")
-            return
+        else:
+            if not a:
+                fields_error()
+                return
             
-            
-    elif method == "False Position":
-        a = a_entry.get()
-        b = b_entry.get()
-        if not a or not b:
-            fields_error()
-            return
         a = float(a)
         b = float(b)
+    except Exception as e:
+        result_label.config(text="Error: {}".format(e), fg="red")
+        error_label.config(text="")
+        info_label.config(text="")
+
+
+    if method == "Bisection":
+        result, error , msg = bisection(f, a, b, float(tolerance), int(max_iterations))              
+    elif method == "False Position":
         result, error, msg= false_position(f, a, b, float(tolerance), int(max_iterations))
     elif method == "Secant":
-        x0 = x0_entry.get()
-        x1 = x1_entry.get()
-        if not x0 or not x1:
-            fields_error()
-            return
-        x0 = float(x0)
-        x1 = float(x1)
-        result, error, msg = secant(f, x0, x1, float(tolerance), int(max_iterations))
+        result, error, msg = secant(f, a, b, float(tolerance), int(max_iterations))
     elif method == "Newton-Raphson":
-        x0 = x0_entry.get()
-        if not x0:
-            fields_error()
-            return
-        x0 = float(x0)
-        result, error, msg = newton_raphson(f, x0, float(tolerance), int(max_iterations))
+        result, error, msg = newton_raphson(f, a, float(tolerance), int(max_iterations))
     elif method == "Simple Fixed-Point Iteration":
-        x0 = x0_entry.get()
-        if not x0:
-            fields_error()
-            return
-        x0 = float(x0)
         try:
-            result, error, msg  = simple_fixed_point_iteration(f, x0, float(tolerance), int(max_iterations))
+            result, error, msg  = simple_fixed_point_iteration(f, a, float(tolerance), int(max_iterations))
         except OverflowError:
             result_label.config(text="Overflow error: The result is too large", fg="red")
             error_label.config(text="")
@@ -103,16 +85,17 @@ def calculate_root():
 def on_method_change(*args):
     method = method_var.get()
     
-    a_label.config(text="Lower Bound (a):" if method in ["Bisection", "False Position"] else "")
-    b_label.config(text="Upper Bound (b):" if method in ["Bisection", "False Position"] else "")
-    x0_label.config(text="Initial Approximation (x0):" if method in ["Secant", "Newton-Raphson", "Simple Fixed-Point Iteration"] else "")
-    x1_label.config(text="Initial Approximation (x1):" if method == "Secant" else "")
-    
-    a_entry.config(state=tk.NORMAL if method in ["Bisection", "False Position"] else tk.DISABLED)
-    b_entry.config(state=tk.NORMAL if method in ["Bisection", "False Position"] else tk.DISABLED)
-    x0_entry.config(state=tk.NORMAL if method in ["Secant", "Newton-Raphson", "Simple Fixed-Point Iteration"] else tk.DISABLED)
-    x1_entry.config(state=tk.NORMAL if method == "Secant" else tk.DISABLED)
-
+    a_label.config(text="Lower Bound (a):" if method in ["Bisection", "False Position"] else "Initial Approximation (x0):")
+    if method in ["Bisection", "False Position"]:
+        b_label.config(text="Upper Bound (b):")
+        b_entry.config(state=tk.NORMAL)
+    elif method == "Secant":
+        b_label.config(text="Initial Approximation (x1):")
+        b_entry.config(state=tk.NORMAL)
+    else:
+        b_label.config(text="")
+        b_entry.config(state=tk.DISABLED)
+        
 # Create the main window
 root = tk.Tk()
 root.title("Numerical Methods Calculator")
@@ -154,16 +137,6 @@ b_label.grid(row=4, column=2)
 b_entry = Entry(root)
 b_entry.grid(row=4, column=3)
 b_entry.insert(0, "5")
-
-x0_label = Label(root, text="")
-x0_label.grid(row=5, column=0)
-x0_entry = Entry(root, state=tk.DISABLED)
-x0_entry.grid(row=5, column=1)
-
-x1_label = Label(root, text="")
-x1_label.grid(row=5, column=2)
-x1_entry = Entry(root, state=tk.DISABLED)
-x1_entry.grid(row=5, column=3)
 
 spacer_label = Label(root, text="")
 spacer_label.grid(row=6, column=0, columnspan=4)
